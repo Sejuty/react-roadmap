2a4b1744b88450548aceea8d346e0e438ccfdc71
Sejuty <tafannumnishat00@gmail.com>
2024-08-26 23:08:23 +0600

    react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+// import App from "./App.jsx";
+// import './index.css'
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "../src/reactRouter/index.css";
+import Root, { loader as rootLoader } from "./reactRouter/routes/root";
+import ErrorPage from "./reactRouter/errorPage";
+import Contact from "./reactRouter/routes/contact";
+ 
+const router = createBrowserRouter([
+  {
+    path: "",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
