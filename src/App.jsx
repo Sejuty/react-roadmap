92565ba5abe3db00aa0490e67ec17a801cc27eb9
Sejuty <tafannumnishat00@gmail.com>
2024-08-27 21:57:48 +0600

    dynamic and nested routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,14 @@ import "./App.css";
 // import CustomHook from "./customHook";
 import { Layout } from "./reactRouter6/layout";
 import { Routes, Route } from "react-router-dom";
-import { Home, Users, NoMatch } from "./reactRouter6";
+import { Home, Users, NoMatch, User } from "./reactRouter6";
 // import UseCallback from "./useCallback";
 // import UseMemo from "./useMemo";
+
+const users = [
+  { id: "1", fullName: "Robin Wieruch" },
+  { id: "2", fullName: "Sarah Finnley" },
+];
 
 function App() {
   return (
@@ -13,7 +18,9 @@ function App() {
         <Route element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
-          <Route index path="users" element={<Users />} />
+          <Route path="users" element={<Users users={users}/>} >
+            <Route path=":userId" element={<User/>}/>
+          </Route>
           <Route path="*" element={<NoMatch />} />
         </Route>
       </Routes>
