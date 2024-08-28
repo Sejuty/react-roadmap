2911301df29300172ae46b3947f3d780c5319862
Sejuty <tafannumnishat00@gmail.com>
2024-08-28 23:16:14 +0600

    programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 // import CustomHook from "./customHook";
 import { Layout } from "./reactRouter6/layout";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Home, Users, NoMatch, User, Profile, Account } from "./reactRouter6";
 // import UseCallback from "./useCallback";
 // import UseMemo from "./useMemo";
 
-const users = [
-  { id: "1", fullName: "Robin Wieruch" },
-  { id: "2", fullName: "Sarah Finnley" },
-];
+// const users = [
+//   { id: "1", fullName: "Robin Wieruch" },
+//   { id: "2", fullName: "Sarah Finnley" },
+// ];
 
 function App() {
+  const [users, setUsers] = useState([
+    { id: "1", fullName: "Robin Wieruch" },
+    { id: "2", fullName: "Sarah Finnley" },
+  ]);
+
+  const navigate = useNavigate();
+
+  const handleRemoveUser = (userId) => {
+    setUsers((state) => state.filter((user) => user.id !== userId));
+    navigate("/users");
+  };
   return (
     <>
       <Routes>
@@ -19,7 +31,13 @@ function App() {
           <Route index element={<Home />} />
           <Route path="home" element={<Home />} />
           {/* asterisk (*) means this route and its component render descendant routes */}
-          <Route path="users/*" element={<Users users={users} />} />
+          <Route path="users" element={<Users users={users} />}>
+            <Route
+              path=":userId"
+              element={<User onRemoveUser={handleRemoveUser} />}
+            />
+          </Route>
+
           {/* <Route path="user" element={<User />}>
             <Route index element={<Profile />} />
             <Route path="profile" element={<Profile />} />
--- src/reactRouter6/index.jsx
+++ src/reactRouter6/index.jsx
@@ -1,4 +1,11 @@
-import { Link, Outlet, Route, Routes, useParams } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  Route,
+  Routes,
+  useParams,
+  useNavigate,
+} from "react-router-dom";
 
 const users = [
   { id: "1", fullName: "Robin Wieruch" },
@@ -17,7 +24,7 @@ export const Users = ({ users }) => {
   return (
     <>
       <h2>Users</h2>
-      <Routes>
+      {/* <Routes>
         <Route
           index
           element={
@@ -31,13 +38,21 @@ export const Users = ({ users }) => {
           }
         />
         <Route path=":userId" element={<User />} />
-      </Routes>
+      </Routes> */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/users/${user.id}`}>{user.fullName}</Link>
+          </li>
+        ))}
+      </ul>
+
       <Outlet />
     </>
   );
 };
 
-export const User = () => {
+export const User = ({ onRemoveUser }) => {
   const { userId } = useParams();
   const user = users.find((user) => user.id === userId);
 
@@ -52,6 +67,9 @@ export const User = () => {
         <Link to="account">Account</Link>
       </nav>
       <Outlet/> */}
+      <button type="button" onClick={() => onRemoveUser(userId)}>
+        Remove
+      </button>
       <h1>{userId}</h1>
       <div>{user.fullName}</div>
       <Link to="/users">Back to users</Link>
